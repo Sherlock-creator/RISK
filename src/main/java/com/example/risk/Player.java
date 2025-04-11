63fb42d96d13011a9294836a1a2cfbf965be2781
RubiusTwon <noah.jones.9@icloud.com>
2025-04-11 17:23:13 -0400

    Javadoc on Territory A little javadoc on Player

--- src/main/java/com/example/risk/Player.java
+++ src/main/java/com/example/risk/Player.java
@@ -4,35 +4,67 @@ import javafx.scene.paint.Color;
 import java.util.HashSet;
 
 /**
- *
+ * Represents a player in the game.
  * @author Bryce Peterson and Noah Jones
  */
 public class Player {
 
+    /**
+     * Identifying string, can be the player's name
+     */
     private String id;
 
+    /**
+     * Color that represents the player on the board
+     */
     private Color color;
 
+    /**
+     * Gives the player's methods access to the board the player's in
+     */
     private Board board;
 
+    /**
+     * Standard constructor, directly takes id and color and sets them as private variables
+     * @param id
+     * @param color
+     */
     public Player(String id, Color color){
         this.id = id;
         this.color = color;
     }
 
+    /**
+     * Returns id
+     * @return id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns color
+     * @return color
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Fetches from the main board which territories are controlled by the player
+     * @return set of controlled territories
+     */
     public HashSet<Territory> getControlledTerritories(){
+        // TODO
         return new HashSet<>();
     }
 
+    /**
+     * Either calculates how many total troops the player can have or how many the player can deploy this turn
+     * @return number of troops
+     */
     public int getDeployCount(){
+        // TODO
         return -1;
     }
 }
--- src/main/java/com/example/risk/Territory.java
+++ src/main/java/com/example/risk/Territory.java
@@ -8,44 +8,90 @@ import java.util.HashSet;
  */
 public class Territory {
 
+    /**
+     * Number of troops in this territory
+     */
     private int troops;
 
+    /**
+     * Player that controls this territory
+     */
     private Player player;
 
+    /**
+     * X position of this territory in the window
+     */
     private double x;
 
+    /**
+     * Y position of this territory in the window
+     */
     private double y;
 
+    /**
+     * Set of all neighboring territories
+     */
     private HashSet<Territory> neighbors;
 
+    /**
+     * Standard constructor, x and y are directly set as private variables
+     * Neighbors and player are initially null, they will be added by Board
+     * @param x
+     * @param y
+     */
     public Territory(double x, double y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns player that controls this territory
+     * @return player
+     */
     public Player getPlayer() {
         return player;
     }
 
+    /**
+     * Returns number of troops in this territory
+     * @return troops
+     */
     public int getTroops(){
         return troops;
     }
 
+    /**
+     * Returns x position of this territory in the window
+     * @return x
+     */
     public double getX() {
         return x;
     }
 
+    /**
+     * Return y position of this territory in the window
+     * @return y
+     */
     public double getY() {
         return y;
     }
 
+    /**
+     * Return set of all neighboring territories
+     * @return neighbors
+     */
     public HashSet<Territory> getNeighbors() {
         return neighbors;
     }
 
+    /**
+     *
+     * @param player
+     */
     public void setPlayer(Player player) {
         this.player = player;
     }
+
 
     public void setTroops(int troops) {
         this.troops = troops;
