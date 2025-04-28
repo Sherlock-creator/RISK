0e3f14a030f330a0c5956043bdae2cc912d9d77b
PETERSONBE24 <Peterson2024@gcc.edu>
2025-04-28 14:12:50 -0400

    Half working button

--- src/main/java/com/example/risk/HelloApplication.java
+++ src/main/java/com/example/risk/HelloApplication.java
@@ -1,18 +1,40 @@
 package com.example.risk;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
 
+import java.awt.*;
 import java.io.IOException;
+import java.sql.SQLOutput;
 
 public class HelloApplication extends Application {
+
+	Button button;
+
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-		stage.setTitle("Hello!");
+//		FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//		Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//		stage.setTitle("Hello!");
+//		stage.setScene(scene);
+//		stage.show();
+		stage.setTitle("Risk");
+		button = new Button();
+		button.setText("This Is a Button");
+		button.setOnAction(e -> System.out.println("balls"));
+
+		Group layout = new Group();
+		layout.getChildren().add(button);
+
+		Scene scene = new Scene(layout,500,500);
 		stage.setScene(scene);
 		stage.show();
 	}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,8 @@ module com.example.risk {
     requires com.almasb.fxgl.all;
 	requires org.junit.jupiter.api;
 	requires java.desktop;
+    requires java.sql;
 
-	opens com.example.risk to javafx.fxml;
+    opens com.example.risk to javafx.fxml;
     exports com.example.risk;
 }
